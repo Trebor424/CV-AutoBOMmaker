933ce11f5f2049ac7ca447c7bda80af72e2294dd
Robert Pawelski <robert.pawelski97@gmail.com>
2025-05-19 20:06:13 +0200

    Initial commit

---
+++ AutoBOMmaker 1.06/AutoBOMmaker/EXCEL_Datagrids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBOMmaker
+{
+    class EXCEL_Datagrids
+    {
+    }
+}
---
+++ AutoBOMmaker 1.06/AutoBOMmaker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelDataReader;
+using System.IO;
+using Microsoft.SqlServer;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AutoBOMmaker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+
+        }
+    }
+}
